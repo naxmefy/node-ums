61adc69e6cfe5457f67e1e733ec44657fc5a695c
MRW Neundorf <n@gkngbr.de>
2016-05-22 17:17:15 +0200

    added wercker.yml

--- app/config.js
+++ app/config.js
@@ -1,40 +1,47 @@
 'use strict';
 
 const sailsDisk = require('sails-disk');
-const sailsMysql = require('sails-mysql');
+// const sailsMysql = require('sails-mysql');
+const sailsPostgresql = require('sails-postgresql');
 
 module.exports = {
     ip: process.env.IP || '',
     port: process.env.PORT || 3000,
-    
+
     log: {
         name: process.env.LOG_NAME || 'ums',
         level: process.env.LOG_LEVEL || 'debug'
     },
-    
+
     waterline: {
         adapters: {
             disk: sailsDisk,
-            mysql: sailsMysql
+            // mysql: sailsMysql,
+            postgre: sailsPostgresql
         },
-        
+
         connections: {
             disk: {
                 adapter: 'disk'
             },
-            
-            mysql: {
-                adapter: 'mysql',
+
+            // mysql: {
+            //     adapter: 'mysql',
+            //     url: process.env.DATABASE_URL
+            // },
+
+            postgre: {
+                adapter: 'postgre',
                 url: process.env.DATABASE_URL
             }
         },
-        
+
         defaults: {
             migrate: process.env.NODE_ENV !== 'production' ? 'alter' : 'safe'
         },
-        
+
         modelDefaults: {
-            connection: process.env.NODE_ENV !== 'production' ? 'disk' : 'mysql'
+            connection: process.env.NODE_ENV !== 'production' ? 'disk' : 'postgre'
         }
     }
 };
